ca2e3112a32d23015b30f83af102df0324bb3953
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 15:27:59 +0900

    logtime

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import weather
 import csv
+import datetime
 
 def main():
 	past=[]
@@ -11,6 +12,9 @@ def main():
 			past.append(weather.AmeDAS(l[1],l[0]))
 			forecast.append(weather.forecast(past[len(past)-1].all[1]))
 		f.close()
+		
+	with open("logtime.txt",mode='w') as f:
+		f.write(dt_now.strftime("%Y/%m/%d %H:%M:%S"))
 
 if __name__=='__main__':
 	main()
